7c4e40d38259b4f64a680a77be76800124c1f9d0
DawonJeong <96712564+JDonlyJD@users.noreply.github.com>
2023-01-24 17:04:31 +0900

    Initial commit

---
+++ src/main/java/com/dotjaling/dotjaling/DotjalingApplication.java
@@ -0,0 +1,13 @@
+package com.dotjaling.dotjaling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotjalingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotjalingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dotjaling/dotjaling/DotjalingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dotjaling.dotjaling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotjalingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
