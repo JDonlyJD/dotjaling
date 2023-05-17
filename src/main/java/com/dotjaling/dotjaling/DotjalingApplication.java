8b147ee19fa7bb4a6f2fb959ef2d1cb26edac4e5
cskim734 <67889952+cskim734@users.noreply.github.com>
2023-05-17 23:03:08 +0900

    템플릿 적용 (#6)
    * 템플릿 적용
    
    템플릿 적용
    
    * 다원이가 하라한 샘플
    
    다원이가 하라한 샘플

--- src/main/java/com/dotjaling/dotjaling/DotjalingApplication.java
+++ src/main/java/com/dotjaling/dotjaling/DotjalingApplication.java
@@ -2,19 +2,12 @@ package com.dotjaling.dotjaling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class DotjalingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DotjalingApplication.class, args);
 	}
 
-	@GetMapping("/")
-	public String home() {
-		return "hello dotjalinig";
-	}
 }
